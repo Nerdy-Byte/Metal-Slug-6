26e76bc08bb329e7348599bba71aab63af9d37fa
Divyanshu Dwivedi <divyanshudwivedi2018gmail.com@Divyanshus-MacBook-Air.local>
2024-08-30 21:10:28 +0530

    Player movement added(jump and run), Camera follows player added

---
+++ Assets/scripts/PlayerControler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public Animator anim;
+    public float moveSpeed;
+    public float jumpForce;
+    private bool onGround;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
+        if(theRB.velocity.x < 0){transform.localScale = new Vector3(-1, 1, 1);}
+        else if(theRB.velocity.x > 0){transform.localScale = new Vector3(1, 1, 1);}
+        onGround = Physics2D.OverlapCircle(groundCheck.position, .2f, whatIsGround);
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+        anim.SetBool("onGround", onGround);
+        anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+    }
+}
